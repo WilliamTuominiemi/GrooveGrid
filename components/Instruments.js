816b4832a31ebf6733059e8137b909931f4b9770
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-02-19 18:56:13 +0200

    new components

---
+++ components/Instruments.js
@@ -0,0 +1,15 @@
+'use client';
+
+const Instruments = () => {
+  return (
+    <div className="grid grid-cols-1 place-content-evenly gap-y-5 bg-softSkyBlue p-4 m-1 ">
+      <p>🎸</p>
+      <p>🎻</p>
+      <p>🥁</p>
+      <p>🎷</p>
+      <p>📯</p>
+    </div>
+  );
+};
+
+export default Instruments;
