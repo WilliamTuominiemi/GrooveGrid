89ed1b56aabf64c35aed2be67d949bd349148f8a
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-03-24 15:06:09 +0200

    Share creation page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
 
 import Canvas from '@/components/Canvas';
 import Instruments from '@/components/Instruments';
@@ -8,6 +10,8 @@ import Controls from '@/components/Controls';
 import Mixer from '@/components/Mixer';
 
 export default function Home() {
+  const { data: session, status } = useSession();
+
   const [notes, setNotes] = useState([]);
   const [activeMix, setActiveMix] = useState(0);
   const [mix, setMix] = useState([
@@ -20,6 +24,42 @@ export default function Home() {
   const [instrumentBoard, setInstrumentBoard] = useState([0, 1, 2, 3, 4]);
 
   const instruments = ['🎹', '🥁', '🎷', '🎸', '🎻', '📯', '🪕', '📢', '🔔', '💥', '💵', '👏', '🎺', '🎤'];
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [title, setTitle] = useState('');
+
+  const generateRandomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const [color1, setColor1] = useState(() => generateRandomColor());
+  const [color2, setColor2] = useState(() => generateRandomColor());
+
+  const handleTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleColor1Change = (event) => {
+    setColor1(event.target.value);
+  };
+
+  const handleColor2Change = (event) => {
+    setColor2(event.target.value);
+  };
+
+  const gradientStyle = {
+    background: `linear-gradient(to right, ${color1}, ${color2})`,
+  };
+
+  const openShareMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+    console.log(isMenuOpen);
+  };
 
   const updateNotes = (newNotes) => {
     setNotes(newNotes);
@@ -49,26 +89,74 @@ export default function Home() {
 
   return (
     <main className="flex items-center justify-center mt-10">
-      <div className="flex flex-col">
-        <div className="flex">
-          <Instruments
-            onUpdateActiveMix={updateActiveMix}
+      {isMenuOpen ? (
+        <div className="grid grid-cols-2 bg-softSkyBlue rounded-md">
+          <form className="grid grid-cols-1 place-content-evenly gap-y-5 bg-powderBlue p-4 m-1 rounded-md justify-items-stretch">
+            <label className="justify-self-center">Publish your work</label>
+            <input
+              className="justify-self-center bg-mutedLavenderBlue p-1 rounded-sm"
+              type="text"
+              id="title"
+              maxLength="11"
+              required
+              value={title}
+              onChange={handleTitleChange}
+              placeholder="Title"
+            />
+            <div className="grid grid-cols-2 justify-items-stretch">
+              <input
+                className="justify-self-center rounded-md bg-mutedLavenderBlue"
+                type="color"
+                id="color1"
+                value={color1}
+                onChange={handleColor1Change}
+              />
+              <input
+                className="justify-self-center rounded-md bg-mutedLavenderBlue"
+                type="color"
+                id="color2"
+                value={color2}
+                onChange={handleColor2Change}
+              />
+            </div>
+
+            <div className="grid grid-cols-2 content-center">
+              <button className="bg-mutedLavenderBlue hover:bg-DarkSoftSkyBlue text-white font-bold py-2 px-4 mx-1 rounded-full focus:outline-none focus:shadow-outline flex justify-center">
+                <Image src="/icons/back.svg" alt="Share Icon" width={15} height={15} />
+              </button>
+              <button className="bg-mutedLavenderBlue hover:bg-DarkSoftSkyBlue text-white font-bold py-2 px-4 mx-1 rounded-full focus:outline-none focus:shadow-outline flex justify-center">
+                <Image src="/icons/share.svg" alt="Share Icon" width={15} height={15} />
+              </button>
+            </div>
+          </form>
+          <div className="p-4 m-1 rounded-md flex flex-col items-center" style={gradientStyle}>
+            <h1 className="text-6xl m-2">💿</h1>
+            <h2 className="text-3xl m-2">{title}</h2>
+            <p className="text-md m-2">{session.user.name}</p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col">
+          <div className="flex">
+            <Instruments
+              onUpdateActiveMix={updateActiveMix}
+              instrumentBoard={instrumentBoard}
+              instruments={instruments}
+            />
+            <Canvas onUpdateNotes={updateNotes} />
+            <Controls notes={notes} mix={mix} instrumentBoard={instrumentBoard} onOpenShareMenu={openShareMenu} />
+          </div>
+          <Mixer
+            onUpdatePitch={updatePitch}
+            onUpdateVolume={updateVolume}
+            active={activeMix}
+            mix={mix}
+            onUpdateInstrumentBoard={updateInstrumentBoard}
             instrumentBoard={instrumentBoard}
             instruments={instruments}
           />
-          <Canvas onUpdateNotes={updateNotes} />
-          <Controls notes={notes} mix={mix} instrumentBoard={instrumentBoard} />
         </div>
-        <Mixer
-          onUpdatePitch={updatePitch}
-          onUpdateVolume={updateVolume}
-          active={activeMix}
-          mix={mix}
-          onUpdateInstrumentBoard={updateInstrumentBoard}
-          instrumentBoard={instrumentBoard}
-          instruments={instruments}
-        />
-      </div>
+      )}
     </main>
   );
 }
--- components/Controls.js
+++ components/Controls.js
@@ -7,10 +7,11 @@ import Image from 'next/image';
 
 import { play, stop } from '@/utils/buttonUtils';
 
-const Controls = ({ notes, mix, instrumentBoard }) => {
+const Controls = ({ notes, mix, instrumentBoard, onOpenShareMenu }) => {
   const router = useRouter();
   const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const share = async () => {
     setSubmitting(true);
@@ -38,24 +39,26 @@ const Controls = ({ notes, mix, instrumentBoard }) => {
 
   return (
     <div className="grid grid-cols-1 place-content-evenly gap-y-5 bg-softSkyBlue p-4 m-1">
-      <button
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-        onClick={() => play(notes, mix, instrumentBoard)}
-      >
-        <Image src="/icons/play.svg" alt="Play Icon" width={10} height={10} />
-      </button>
-      <button
-        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-        onClick={stop}
-      >
-        <Image src="/icons/stop.svg" alt="Stop Icon" width={10} height={10} />
-      </button>
-      <button
-        className="bg-white hover:bg-gray-200 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
-        onClick={share}
-      >
-        <Image src="/icons/share.svg" alt="Share Icon" width={10} height={10} />
-      </button>
+      <>
+        <button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+          onClick={() => play(notes, mix, instrumentBoard)}
+        >
+          <Image src="/icons/play.svg" alt="Play Icon" width={10} height={10} />
+        </button>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+          onClick={stop}
+        >
+          <Image src="/icons/stop.svg" alt="Stop Icon" width={10} height={10} />
+        </button>
+        <button
+          className="bg-white hover:bg-gray-200 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline"
+          onClick={() => onOpenShareMenu()}
+        >
+          <Image src="/icons/share.svg" alt="Share Icon" width={10} height={10} />
+        </button>
+      </>
     </div>
   );
 };
